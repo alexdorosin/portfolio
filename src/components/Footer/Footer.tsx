8fe9fbeb0256f6bb10f4de4b14fb13e9e1b92c14
Alex Dorosin <alex.dorosin@revolut.com>
2024-02-03 16:58:14 +0200

    initial setup

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,63 @@
+import "./Footer.css"
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__container container">
+        <h1 className="footer__title">Smith</h1>
+
+        <ul className="footer__list">
+          <li>
+            <a href="#about" className="footer__link">
+              About
+            </a>
+          </li>
+
+          <li>
+            <a href="#portfolio" className="footer__link">
+              Projects
+            </a>
+          </li>
+
+          <li>
+            <a href="#testimonials" className="footer__link">
+              Testimonials
+            </a>
+          </li>
+        </ul>
+
+        <div className="footer__social">
+          <a
+            href="https://facebook.com/"
+            className="footer__social-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="bx bxl-facebook" />
+          </a>
+          <a
+            href="https://instagram.com/"
+            className="footer__social-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="bx bxl-instagram" />
+          </a>
+
+          <a
+            href="https://twitter.com/"
+            className="footer__social-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="bx bxl-twitter" />
+          </a>
+        </div>
+
+        <span className="footer__copy">
+          &#169; Crypticalcoder. All right reserved
+        </span>
+      </div>
+    </footer>
+  )
+}
